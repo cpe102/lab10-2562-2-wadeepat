c8534b8cd64e865daf720efff2f57fc583b57310
lab314-10@labeng.local <lab314-10@labeng.local>
2020-01-31 16:27:22 +0700

    finished

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,26 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+    string a;
+    
+    cout << "Press Enter 3 times to reveal your future.";
+    for(int i=0;i<3;i++)
+        getline(cin,a);
+    
+    srand(time(0));
+    int x = rand()%9;
+    if(x==0) a="A";
+    else if(x==1) a="B+";
+    else if(x==2) a="B";
+    else if(x==3) a="C";
+    else if(x==4) a="C+";
+    else if(x==5) a="D";
+    else if(x==6) a="D+";
+    else if(x==7) a="F";
+    else a="W";
+    
+    cout << "You will get "<<a<<" in this 261102.";
+}
